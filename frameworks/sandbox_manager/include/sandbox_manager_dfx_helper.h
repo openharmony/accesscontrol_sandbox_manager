e0a25b2ad3ceb95860897e3b087da76afb3a6ea5
GengYinzong <gengyinzong@huawei.com>
2024-08-06 06:33:30 -0700

    add dfx info
    Signed-off-by: GengYinzong <gengyinzong@huawei.com>

---
+++ frameworks/sandbox_manager/include/sandbox_manager_dfx_helper.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SANDBOX_MANAGER_DFX_HELPER_H
+#define SANDBOX_MANAGER_DFX_HELPER_H
+#include <cstdint>
+#include <map>
+#include <string>
+
+namespace OHOS {
+namespace AccessControl {
+namespace SandboxManager {
+enum OperateTypeEnum: int32_t {
+    // temporary policy
+    SET_POLICY,
+    UNSET_ALL_POLICY_BY_TOKEN,
+    UNSET_POLICY,
+    // persist policy
+    CLEAN_PERSIST_POLICY_BY_PATH,
+    PERSIST_POLICY,
+    REMOVE_BUNDLE_POLICY_BY_EVENT,
+    START_ACCESSING_POLICY,
+    START_ACCESSING_POLICY_BY_TOKEN,
+    STOP_ACCESSING_POLICY,
+    UNPERSIST_POLICY,
+};
+
+const static std::map<int32_t, std::string> OPERATE_TYPE_MAP = {
+    // temporary policy
+    {OperateTypeEnum::SET_POLICY, "set_policy"},
+    {OperateTypeEnum::UNSET_ALL_POLICY_BY_TOKEN, "unset_all_policy_by_token"},
+    {OperateTypeEnum::UNSET_POLICY, "unset_policy"},
+    // persist policy
+    {OperateTypeEnum::CLEAN_PERSIST_POLICY_BY_PATH, "clean_persist_policy_by_path"},
+    {OperateTypeEnum::PERSIST_POLICY, "persist_policy"},
+    {OperateTypeEnum::REMOVE_BUNDLE_POLICY_BY_EVENT, "remove_bundle_policy_by_event"},
+    {OperateTypeEnum::START_ACCESSING_POLICY, "start_accessing_policy"},
+    {OperateTypeEnum::START_ACCESSING_POLICY_BY_TOKEN, "start_accessing_policy_by_token"},
+    {OperateTypeEnum::STOP_ACCESSING_POLICY, "stop_accessing_policy"},
+    {OperateTypeEnum::UNPERSIST_POLICY, "unpersist_policy"},
+};
+
+class PolicyOperateInfo {
+public:
+    PolicyOperateInfo(
+        uint32_t totalNum, uint32_t successNum,
+        uint32_t failNum, uint32_t invalidNum);
+
+    uint32_t callerPid;
+    uint32_t callerTokenid;
+    uint32_t policyNum;
+    uint32_t successNum;
+    uint32_t failNum;
+    uint32_t invalidNum;
+};
+
+class SandboxManagerDfxHelper {
+public:
+    static void WritePermissionCheckFailEvent(const std::string &permission,
+        const uint32_t callerTokenid = 0, const uint32_t callerPid = 0);
+    static void WritePersistPolicyOperateSucc(const OperateTypeEnum operateType, const PolicyOperateInfo &info);
+    static void WriteTempPolicyOperateSucc(const OperateTypeEnum operateType, const PolicyOperateInfo &info);
+};
+} // SandboxManager
+} // AccessControl
+} // OHOS
+#endif // SANDBOX_MANAGER_DFX_HELPER_H
---
+++ frameworks/sandbox_manager/src/sandbox_manager_dfx_helper.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "sandbox_manager_dfx_helper.h"
+
+#include "hisysevent.h"
+#include "ipc_skeleton.h"
+
+namespace OHOS {
+namespace AccessControl {
+namespace SandboxManager {
+
+static std::string GetOperateString(const OperateTypeEnum operateType)
+{
+    auto it = OPERATE_TYPE_MAP.find(operateType);
+    if (it == OPERATE_TYPE_MAP.end()) {
+        return "unknown_type";
+    }
+    return it->second;
+}
+
+PolicyOperateInfo::PolicyOperateInfo(uint32_t totalNum, uint32_t successNum,
+    uint32_t failNum, uint32_t invalidNum) : policyNum(totalNum),
+    successNum(successNum), failNum(failNum), invalidNum(invalidNum)
+{
+    callerPid = IPCSkeleton::GetCallingRealPid();
+    callerTokenid = IPCSkeleton::GetCallingTokenID();
+}
+
+void SandboxManagerDfxHelper::WritePermissionCheckFailEvent(const std::string &permission,
+    const uint32_t callerTokenid, const uint32_t callerPid)
+{
+    uint32_t inputPid = callerPid;
+    if (inputPid == 0) {
+        inputPid = IPCSkeleton::GetCallingRealPid();
+    }
+    uint32_t inputTokenid = callerTokenid;
+    if (inputTokenid == 0) {
+        inputTokenid = IPCSkeleton::GetCallingTokenID();
+    }
+    HiSysEventWrite(HiviewDFX::HiSysEvent::Domain::SANDBOX_MANAGER, "CALLER_PERMISSION_CHECK_FAILED",
+        HiviewDFX::HiSysEvent::EventType::FAULT, "CALLER_PID", inputPid,
+        "CALLER_TOKENID", inputTokenid, "PERMISSION_NAME", permission);
+}
+
+void SandboxManagerDfxHelper::WritePersistPolicyOperateSucc(
+    const OperateTypeEnum operateType, const PolicyOperateInfo &info)
+{
+    std::string type = GetOperateString(operateType);
+    HiSysEventWrite(HiviewDFX::HiSysEvent::Domain::SANDBOX_MANAGER, "PERSIST_POLICY_OPERATE_SUCCESS",
+        HiviewDFX::HiSysEvent::EventType::FAULT, "OPERATE_TYPE", type,
+        "CALLER_PID", info.callerPid, "CALLER_TOKENID", info.callerTokenid,
+        "TOTAL_NUM", info.policyNum, "SUCCESS_NUM", info.successNum,
+        "FAIL_NUM", info.failNum, "INVALID_NUM", info.invalidNum);
+}
+
+void SandboxManagerDfxHelper::WriteTempPolicyOperateSucc(
+    const OperateTypeEnum operateType, const PolicyOperateInfo &info)
+{
+    uint32_t type = static_cast<uint32_t>(operateType);
+    HiSysEventWrite(HiviewDFX::HiSysEvent::Domain::SANDBOX_MANAGER, "TEMPORARY_POLICY_OPERATE_SUCCESS",
+        HiviewDFX::HiSysEvent::EventType::FAULT, "OPERATE_TYPE", type,
+        "CALLER_PID", info.callerPid, "CALLER_TOKENID", info.callerTokenid,
+        "TOTAL_NUM", info.policyNum, "SUCCESS_NUM", info.successNum,
+        "FAIL_NUM", info.failNum, "INVALID_NUM", info.invalidNum);
+}
+}
+}
+}
