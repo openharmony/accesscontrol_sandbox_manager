63c6075121b857f5e36477eac689ebceb84a30a8
blackyinzong <gengyinzong@h-partners.com>
2025-10-28 14:57:20 +0800

    update: 更新文件 sandbox_manager_kit_supplemental_test.cpp add fix
    Signed-off-by: blackyinzong <gengyinzong@h-partners.com>

--- frameworks/inner_api/sandbox_manager/test/unittest/src/sandbox_manager_kit_supplemental_test.cpp
+++ frameworks/inner_api/sandbox_manager/test/unittest/src/sandbox_manager_kit_supplemental_test.cpp
@@ -54,6 +54,7 @@ const std::string FILE_ACCESS_PERMISSION = "ohos.permission.FILE_ACCESS_MANAGER"
 const uint32_t INVALID_OPERATE_MODE = 0;
 const double SET_POLICY_MAX_TIME_SEC = 15.0;
 const double CHECK_PERSIST_MAX_TIME_SEC = 200.0 / 1000.0;
+#define TEST_TIMESTAMP 5
 #endif
 const size_t MAX_POLICY_NUM = 8;
 const int DEC_POLICY_HEADER_RESERVED = 64;
